ec1bc9393a0b1da57844acb4fe82776523b07a7d
rbsilva1 <rodrigo.belarmino17@gmail.com>
2024-06-10 10:09:48 -0300

    feat: codigo novo

--- first-question/src/App.java
+++ first-question/src/App.java
@@ -1,7 +1,10 @@
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Scanner;
 
 public class App {
     public static final int NUM_THREADS = 2;
@@ -10,22 +13,27 @@ public class App {
     public static void main(String[] args) throws Exception {
         List<String> phrases = new ArrayList<>();
         long startTime = System.currentTimeMillis();
-        File file = new File("first-question/src/filename.txt");
         Recurrency recurrency = new Recurrency();
-        Scanner reader = new Scanner(file);
 
-        while (reader.hasNextLine()) {
-            String data = reader.nextLine();
-            phrases.add(data);
+        try (BufferedReader br = new BufferedReader(
+                new InputStreamReader(new FileInputStream(new File("first-question/src/teste.txt"))))) {
+
+            String phrase;
+            while ((phrase = br.readLine()) != null) {
+                phrases.add(phrase);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
         }
-
-        reader.close();
 
         int batchSize = phrases.size() / NUM_THREADS;
         int startIndex = 0;
         List<Thread> threads = new ArrayList<>();
         for (int i = 0; i < NUM_THREADS; i++) {
             int endIndex = Math.min(startIndex + batchSize, phrases.size());
+            if (endIndex == phrases.size() - 1 && i == NUM_THREADS - 1) {
+                endIndex = phrases.size();
+            }
             Thread thread = new CountWords(phrases.subList(startIndex, endIndex), recurrency);
             threads.add(thread);
             thread.start();
@@ -40,7 +48,8 @@ public class App {
             }
         }
 
-        recurrency.getWords(NUM_WORDS);
+        recurrency.getWords(3);
+        // recurrency.getWords(NUM_WORDS);
 
         long endTime = System.currentTimeMillis();
 
